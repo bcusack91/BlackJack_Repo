8c154b49b9c47c5b228ec7223b31b83047990fc1
brendan@cusackprep.com <bcusack91@gmail.com>
2021-06-27 21:38:53 -0400

    I've added the user functions, but still need to add the scoring

--- final.py
+++ final.py
@@ -1,3 +1,28 @@
 from deck_of_cards import deck_of_cards
 
 print("Yes, here we go!")
+
+dealer_hand = []
+user_hand = []
+
+i = 0
+while (i<2):
+    dealer_hand.append(deck_of_cards.DeckOfCards().give_random_card())
+    user_hand.append(deck_of_cards.DeckOfCards().give_random_card())
+    i+=1
+
+print("In your hand, you will find:")
+for card in user_hand:
+    print(card.name)
+
+print("And the dealer is showing a ")
+print(dealer_hand[1].name)
+
+user_response = input("Would you like to hit or stick? ")
+
+while (user_response == "hit"):
+    user_hand.append(deck_of_cards.DeckOfCards().give_random_card())
+    print("In your hand, you will find:")
+    for card in user_hand:
+        print(card.name)
+    user_response = input("Would you like to hit or stick? ")
